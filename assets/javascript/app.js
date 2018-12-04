c9bcc224355079c6f9247f3cac10fa19fbf92727
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-12-04 15:21:26 -0500

    updating stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,52 @@
 const questions = [
-    {
-        question: "On what floor of the J. Edgar Hoover FBI Building is Mulder's office?",
-        answers: {
-            a: "The first floor",
-            b: "The basement",
-            c: "The top floor",
-        },
-        correctAnswer: "b",
+  {
+    question:
+      "On what floor of the J. Edgar Hoover FBI Building is Mulder's office?",
+    answers: {
+      a: "The first floor",
+      b: "The basement",
+      c: "The top floor"
     },
-    {
-        question: "What is Mulder's sister's name?",
-        answers: {
-            a: "Amanda",
-            b: "Stepahnie",
-            c: "Samantha",
-        },
-        correctAnswer: "c",
+    correctAnswer: "b"
+  },
+  {
+    question: "What is Mulder's sister's name?",
+    answers: {
+      a: "Amanda",
+      b: "Stepahnie",
+      c: "Samantha"
     },
-    {
-        question: "What item does Scully give to Emily Sim in the episode 'A Christmas Carol'?",
-        answers: {
-            a: "A teddy bear",
-            b: "Her cross necklace",
-            c: "Her baby blanket",
-        },
-        correctAnswer: "b",
+    correctAnswer: "c"
+  },
+  {
+    question:
+      "What item does Scully give to Emily Sim in the episode 'A Christmas Carol'?",
+    answers: {
+      a: "A teddy bear",
+      b: "Her cross necklace",
+      c: "Her baby blanket"
     },
-    {
-        question: "How old was Mulder when his sister was abducted?",
-        answers: {
-            a: "16",
-            b: "11",
-            c: "12",
-        },
-        correctAnswer: "c",
+    correctAnswer: "b"
+  },
+  {
+    question: "How old was Mulder when his sister was abducted?",
+    answers: {
+      a: "16",
+      b: "11",
+      c: "12"
     },
-    {
-        question: "What was the name of the group that made a deal with the aliens to colonize the planet?",
-        answers: {
-            a: "The Syndicate",
-            b: "The Colloquium",
-            c: "The Triumvarate",
-        },
-        correctAnswer: "a",
-    }
+    correctAnswer: "c"
+  },
+  {
+    question:
+      "What was the name of the group that made a deal with the aliens to colonize the planet?",
+    answers: {
+      a: "The Syndicate",
+      b: "The Colloquium",
+      c: "The Triumvarate"
+    },
+    correctAnswer: "a"
+  }
 ];
 const quizContainer = document.getElementById("quiz");
 const submitButton = document.getElementById("submit");
@@ -56,40 +59,38 @@ var timeLeft = 100000;
 var timerId = setInterval(countdown, 1000);
 
 function countdown() {
-    if (timeLeft === 0) {
-        clearTimeout(timerId);
-        scoreQuiz();
-    }
-    else {
-        $(".timer").text(timeLeft + ' secs remaining');
-        timeLeft--;
-    }
+  if (timeLeft === 0) {
+    clearTimeout(timerId);
+    scoreQuiz();
+  } else {
+    $(".timer").text(timeLeft + " secs remaining");
+    timeLeft--;
+  }
 }
 countdown();
 
 function displayQuestions() {
-    const output = [];
-    rightAnswers = 0;
-    wrongAnswers = 0;
-    unAnswered = 0;
+  const output = [];
+  rightAnswers = 0;
+  wrongAnswers = 0;
+  unAnswered = 0;
 
-    questions.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = [];
+  questions.forEach((currentQuestion, questionNumber) => {
+    const answers = [];
 
-            for (letter in currentQuestion.answers) {
-                answers.push(
-                    `<label for="${questionNumber}.${letter}">
-                <input type = "radio" id="${questionNumber}.${letter}" class = "buttons" name="question${questionNumber}" value="${letter}"/>
+    for (letter in currentQuestion.answers) {
+      answers.push(
+        `<label for="${questionNumber}-${letter}">
+                <input type = "radio" id="${questionNumber}-${letter}" class = "buttons" name="question${questionNumber}" value="${letter}"/>
                 <span>
                 ${letter}: ${currentQuestion.answers[letter]}
                 </span>
                 </label>
                 <br>`
-                )
-            }
-            output.push(
-                `
+      );
+    }
+    output.push(
+      `
             <div class="question">${currentQuestion.question}</div>
             <br>
             <div class="answers">
@@ -97,57 +98,52 @@ function displayQuestions() {
             </div>
             <br>
             `
-            );
-        }
-    )
-    quizContainer.innerHTML = `<form id = "myForm">${output.join('')}</form><br><input type="submit" id="submit">`;
+    );
+  });
+  quizContainer.innerHTML = `<form id = "myForm">${output.join(
+    ""
+  )}</form><br><input type="submit" id="submit">`;
 }
 displayQuestions();
 
-$(".buttons").click(function () {
+$(".buttons").click(function() {
+  if (userAnswers.length > 4) {
+    alert("You've answered all the questions!");
+  } else {
+    userAnswers.push($(this).val());
+    console.log(userAnswers);
+  }
+});
 
-    if (userAnswers.length > 4) {
-        alert("You've answered all the questions!")
-    }
-    else {
-        userAnswers.push($(this).val());
-        console.log(userAnswers);
-    }
-})
-
-$("#submit").click(function () {
-    scoreQuiz();
+$("#submit").click(function() {
+  scoreQuiz();
 });
 
 function scoreQuiz() {
-    clearTimeout(timerId);
-    userAnswers.forEach(
-        (e, index) => {
-            if (e === questions[index].correctAnswer) {
-                rightAnswers++;
+  clearTimeout(timerId);
 
-            }
-            else if (e != questions[index].correctAnswer) {
-                wrongAnswers++;
-            }
-            else if (e === "") {
-                unAnswered++;
-            }
-        })
-    console.log(wrongAnswers);
-    console.log(rightAnswers);
-    $(".timer").empty();
-    $("#quiz").html(`
+  userAnswers.forEach((e, index) => {
+    if (e === questions[index].correctAnswer) {
+      rightAnswers++;
+    } else if (e != questions[index].correctAnswer) {
+      wrongAnswers++;
+    }
+  });
+  console.log(wrongAnswers);
+  console.log(rightAnswers);
+  if (rightAnswers + wrongAnswers < 5) {
+    unAnswered = Math.abs(rightAnswers + wrongAnswers - 5);
+  }
+  $(".timer").empty();
+  $("#quiz").html(`
     <div class = "wins">Right Answers: ${rightAnswers}</div>
     <div class = "losses">Wrong Answers: ${wrongAnswers}</div>
     <div class = "losses">Unanswered: ${unAnswered}</div>
     <br>
-    <input type="reset" id="reset">
+    <input type="reset" id="again">
     `);
-};
+}
 
-$("#reset").click(function () {
-    displayQuestions();
-
-})
-
+$("input#again").click(function() {
+  location.reload();
+});
